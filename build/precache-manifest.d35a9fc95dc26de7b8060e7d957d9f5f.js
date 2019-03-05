beab03359d2fc1acc7ec1d87803c1134fd77f8d6
Pedro Piloto <pedro@pedropiloto.com>
2019-03-05 17:31:59 +0000

    deploy

---
+++ build/precache-manifest.d35a9fc95dc26de7b8060e7d957d9f5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4a58432871d6a6644ce",
+    "url": "/static/css/main.fdadfea5.chunk.css"
+  },
+  {
+    "revision": "d4a58432871d6a6644ce",
+    "url": "/static/js/main.d4a58432.chunk.js"
+  },
+  {
+    "revision": "cfcab775a3d4302fde51",
+    "url": "/static/css/1.87519584.chunk.css"
+  },
+  {
+    "revision": "cfcab775a3d4302fde51",
+    "url": "/static/js/1.cfcab775.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55f43111c6c4839f8a5844bc0d5ccc24",
+    "url": "/static/media/logo.55f43111.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "6f592030f6039ee4d0ea72119d5c0037",
+    "url": "/index.html"
+  }
+];
